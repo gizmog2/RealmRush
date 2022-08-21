70673451d6c67f7b755b728740bdcc959f5b86cc
Сергей Буркун <GizmoG2@live.ru>
2022-08-21 20:06:06 +0300

    Прописано плавное движение, вместо скачнов и регулятор скорости.

--- Assets/Scripts/EnemyMover.cs
+++ Assets/Scripts/EnemyMover.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class EnemyMover : MonoBehaviour
 {
     [SerializeField] List<Waypoint> path = new List<Waypoint>();
-    [SerializeField] float waitTime = 1f;
+    [SerializeField][Range(0, 2)] float speed = 1f;
     // Start is called before the first frame update
     void Start()
     {        
@@ -18,9 +18,22 @@ public class EnemyMover : MonoBehaviour
     {
         foreach (Waypoint waypoint in path)
         {
-            transform.position = waypoint.transform.position;
-            //Debug.Log(waypoint.name);
-            yield return new WaitForSeconds(waitTime);
+            Vector3 startPosition = transform.position;
+            Vector3 endPosition = waypoint.transform.position;
+            float travelPercent = 0f;
+
+            transform.LookAt(endPosition);  // направление в которое смотрит обьект
+
+            while (travelPercent < 1f)
+            {
+                travelPercent += Time.deltaTime*speed;
+                transform.position = Vector3.Lerp(startPosition, endPosition, travelPercent);
+                yield return new WaitForEndOfFrame();
+            }
+
+            //transform.position = waypoint.transform.position;
+            //Debug.Log(waypoint.name);            
+            //yield return new WaitForSeconds(waitTime);
         }
     }
 }
