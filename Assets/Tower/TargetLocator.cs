7d628e367b0beecdbeed97fa47adadad2f07d18b
Сергей Буркун <GizmoG2@live.ru>
2022-09-05 22:39:25 +0300

    Установлен асет для скриптов. Настроен автоматический выбор цели.

--- Assets/Tower/TargetLocator.cs
+++ Assets/Tower/TargetLocator.cs
@@ -1,4 +1,4 @@
-using System;
+﻿using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -6,22 +6,67 @@ using UnityEngine;
 public class TargetLocator : MonoBehaviour
 {
     [SerializeField] Transform weapon;
-    [SerializeField] Transform target;
-
+	[SerializeField] ParticleSystem projektileParticles;
+	[SerializeField] float range = 15f;
+	Transform target;
     // Start is called before the first frame update
     void Start()
-    {
-        target = FindObjectOfType<EnemyMover>().transform;
+	{
+	    target = FindObjectOfType<Enemy>().transform;
     }
 
     // Update is called once per frame
     void Update()
-    {
+	{
+		FindClosestTarget();
         AimWeapon();
-    }
+	}
+    
+	void FindClosestTarget()
+	{
+		Enemy[] enemies = FindObjectsOfType<Enemy>();
+		Transform closestTarget = null;
+		float maxDistance = Mathf.Infinity;
+		
+		foreach (Enemy enemy in enemies)
+		{
+			float targetDistance = Vector3.Distance(transform.position, enemy.transform.position);
+			
+			if (targetDistance < maxDistance)
+			{
+				closestTarget = enemy.transform;
+				maxDistance = targetDistance;
+			}
+		}
+		
+		target = closestTarget;
+	}
 
     void AimWeapon()
-    {
-        weapon.transform.LookAt(target);
+	{
+		float targetDistance = Vector3.Distance(transform.position, target.position);
+		weapon.LookAt(target);
+		if (targetDistance < range)
+		{
+			Attack(true);
+		}
+		else
+		{
+			Attack(false);
+		}
     }
+    
+	void Attack(bool isActive)
+	{
+		var	emissionModule = projektileParticles.emission;
+		emissionModule.enabled = isActive;
+		//if (isActive == true)
+		//{
+		//	emissionModule.enabled = isActive;
+		//}
+		//else
+		//{
+		//	emissionModule.enabled = isActive;
+		//}
+	}
 }
