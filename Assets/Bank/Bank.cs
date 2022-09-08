9a2febc6050e8c9a9c13ce82079b07aa3b8432b2
Сергей Буркун <GizmoG2@live.ru>
2022-09-08 19:27:03 +0300

    Добавлен интерфейс для денег.

--- Assets/Bank/Bank.cs
+++ Assets/Bank/Bank.cs
@@ -2,35 +2,52 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using TMPro;
 
 public class Bank : MonoBehaviour
 {
 	[SerializeField] int startingBalance = 150;
 	[SerializeField] int currentBallance;
+	[SerializeField] TextMeshProUGUI balanceText;
 	public int CurrentBalance{ get { return currentBallance;}}
    
 	void Awake()
 	{
 		currentBallance = startingBalance;
-	}
-	
-	
-   
+        DisplayText();
+    }
+
+	/*private void Start()
+	{
+		balanceText = GetComponent<TextMeshProUGUI>();
+		DisplayText();
+		
+	}*/
+
+
+
 	public void Deposit(int amount)
 	{
 		currentBallance += Mathf.Abs(amount);
-	}
+        DisplayText();
+    }
 	
 	public void Withdraw(int amount)
 	{
 		currentBallance -= Mathf.Abs(amount);
+        DisplayText();
 
-		if (currentBallance < 0)
+        if (currentBallance < 0)
 		{
 			//Loss the game;
 			ReloadScene();
 		}
 	}
+
+	void DisplayText()
+	{
+        balanceText.text = "Gold: " + currentBallance;
+    }
 	void ReloadScene()
 	{
 		int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
