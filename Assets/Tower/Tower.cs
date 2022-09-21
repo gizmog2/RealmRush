363ff498b9eb7928f7ce4ec20f80ef92e04b6774
Сергей Буркун <GizmoG2@live.ru>
2022-09-21 18:41:37 +0300

    Прописан таймер для появления частей обьекта.

--- Assets/Tower/Tower.cs
+++ Assets/Tower/Tower.cs
@@ -6,6 +6,14 @@ using UnityEngine;
 public class Tower : MonoBehaviour
 {
     [SerializeField] int cost = 75;
+    [SerializeField] [Range (0f, 5f)] float buildTimer = 1f;
+    
+
+
+    private void Start()
+    {       
+        StartCoroutine(Build());
+    }
     public bool CreateTower(Tower tower, Vector3 position)
     {
         Bank bank = FindObjectOfType<Bank>();
@@ -22,6 +30,27 @@ public class Tower : MonoBehaviour
             return true;
         }
         return false;
+    }
+    IEnumerator Build()
+    {
+        foreach (Transform child in transform)
+        {
+            child.gameObject.SetActive(false);
+            foreach (Transform grandchild in transform)
+            {
+                grandchild.gameObject.SetActive(false);                
+            }           
+        }
+
+        foreach (Transform child in transform)
+        {
+            child.gameObject.SetActive(true);
+            yield return new WaitForSeconds(buildTimer);
+            foreach (Transform grandchild in transform)
+            {
+                grandchild.gameObject.SetActive(true);                
+            }           
+        }
     }
 
     
